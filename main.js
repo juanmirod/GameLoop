349897392ce0799f916e737195ca6f90e1eb2446
juanmirod <juanmi.rodriguez.ceron@gmail.com>
2013-12-15 00:51:53 +0000

    Added Polygon primitive.

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 // Get a reference to the element.
 
-var elem = document.getElementById('myCanvas');
+var elem = document.getElementById('myCanvas')
+  , polygon
+  , debugON = true;
+  
+function addSide(){
+  polygon.numSides++;
+  polygon.update();
+}
   
 // Always check for properties and methods, to make sure your code doesn't break 
 // in other browsers.
@@ -12,20 +19,25 @@ if ( elem && elem.getContext ) {
     
     //Create new Game Object
     var game = new Game(elem.width, elem.height);             
+    
     var pj = new Primitive.Rectangle(0, 0, 10, 10);
     pj.setColor("rgb(200,100,0)");
+    
     var pj2 = new Primitive.Rectangle(5, 20, 10, 10);
     pj2.setColor("rgb(0,200,0)");
+    
+    polygon = new Primitive.Polygon(100, 100, 50, 5);
     
     game.draw = function(){
       
 	    //clear the screen
-	    context.fillStyle="#FFFFFF";
+	    context.fillStyle="#FAFAFA";
       context.fillRect( 0, 0, game.width, game.height );
         
 	    //paints some figures
 	    pj.draw(context);
 	    pj2.draw(context);
+	    polygon.draw(context);
     };
     
     game.update = function(){
--- primitives.js
+++ primitives.js
@@ -16,29 +16,72 @@ var Primitive = (function() {
       // private properties
       var color = "rgb(200,0,0)";
       
-      var pub = {}
+      var pub = {};
       
       pub.x = x;
       pub.y = y;
       pub.width = width;
       pub.height = height;
         
-      pub.draw = function (context) {
-        context.fillStyle = color;
-        context.fillRect(pub.x, pub.y, pub.width, pub.height);
-      },
+      pub.draw = function (ctx) {
+        ctx.fillStyle = color;
+        ctx.fillRect(pub.x, pub.y, pub.width, pub.height);
+      };
                           
       pub.setColor = function (newColor){
         color = newColor;
-      }
+      };
       
       return pub;
       
-    }
+    },
     
     // A Polygon is a closed path that draws a regular polygon of N sides
-    Polygon: function(){
+    Polygon: function(x, y, size, numSides){
       
+      var color = "rgb(0,250,0)";
+      
+      var updatePoints = function(){
+        var angle   = deg2rad(360/pub.numSides);
+        pub.points  = []; 
+        for(var i=0; i<pub.numSides; i++){
+          pub.points[i] = {x: pub.x+(Math.cos(angle*i)*pub.size), y: pub.y+(Math.sin(angle*i)*pub.size) }
+        }
+      };
+      
+      var pub = {};
+      
+      pub.x = x;
+      pub.y = y;
+      pub.size = size;
+      pub.numSides = numSides;
+      
+      pub.draw = function(ctx){
+        ctx.strokeStyle = color;
+        ctx.lineWidth   = "5";
+        ctx.beginPath();
+        for(var index in pub.points){        
+          if(index == 0)
+            ctx.moveTo(pub.points[index].x, pub.points[index].y);
+          else
+            ctx.lineTo(pub.points[index].x, pub.points[index].y);            
+        }
+        ctx.lineTo(pub.points[0].x, pub.points[0].y);        
+        ctx.stroke();
+        if(debugON) ctx.fillRect(pub.x-1, pub.y-1, 2, 2);
+      };
+      
+      pub.setColor = function (newColor){
+        color = newColor;
+      };
+      
+      pub.update = function () {
+        updatePoints();
+      };
+      
+      updatePoints();
+      
+      return pub;      
     }
   };  
 })();
