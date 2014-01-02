9809a5202cd97e846fcbd26ee9fd0bf57e286df6
juanmirod <juanmi.rodriguez.ceron@gmail.com>
2014-01-02 23:38:54 +0000

    improving the Primitive namespace pattern

--- primitives.js
+++ primitives.js
@@ -2,110 +2,103 @@
   I don't like this pattern with inheritance all nested in the same place. 
   Maybe is better just to use the prototype pattern and use this when needed.
 */
+var Primitive = Primitive || {};
 
-
-var Primitive = (function() {
+(function ( Primitive ) {
 
   //private scope of this module
   function deg2rad(ang) {
       return ang * (Math.PI/180.0);
   }
     
-  //exposed properties    
-  return {
+  // A Rectangle is an encapsulation of the canvas.fill to draw rectagles
+  Primitive.Rectangle = function(x, y, width, height){
     
-    // opacity defines if the object is drawable or hidden
-    var opacity = 1;
+    // private properties
+    var color = "rgb(200,0,0)";
+    
+    var pub = {};
+    
+    pub.x = x;
+    pub.y = y;
+    pub.width = width;
+    pub.height = height;
+    
+    return pub;    
+  }
   
-    // A Rectangle is an encapsulation of the canvas.fill to draw rectagles
-    Rectangle: function(x, y, width, height){
-      
-      // private properties
-      var color = "rgb(200,0,0)";
-      
-      var pub = {};
-      
-      pub.x = x;
-      pub.y = y;
-      pub.width = width;
-      pub.height = height;
-        
-      pub.draw = function (ctx) {
-        ctx.fillStyle = color;
-        ctx.fillRect(pub.x, pub.y, pub.width, pub.height);
-      };
-                          
-      pub.setColor = function (newColor){
-        color = newColor;
-      };
-      
-      return pub;
-      
-    },
+  Primitive.Rectangle.prototype.draw = function (ctx) {
+    ctx.fillStyle = pub.color;
+    ctx.fillRect(pub.x, pub.y, pub.width, pub.height);
+  }
+                        
+  Primitive.Rectangle.prototype.setColor = function (newColor){
+    color = newColor;
+  }
     
-    // A Polygon is a closed path that draws a regular polygon of N sides
-    Polygon: function(x, y, size, numSides){
+  // A Polygon is a closed path that draws a regular polygon of N sides
+  Primitive.Polygon = function(x, y, size, numSides){
+    
+    var color       = "rgb(0,250,0)"
+      , colorFill   = "rgb(200,250,200)"
+      , lineWidth   = "3";
       
-      var color       = "rgb(0,250,0)"
-        , colorFill   = "rgb(200,250,200)"
-        , lineWidth   = "3";
-        
-      
-      var updatePoints = function(){
-        var angle   = deg2rad(360/pub.numSides);
-        var initAngle = deg2rad(pub.rotateAngle);
-        pub.points  = []; 
-        for(var i=0; i<pub.numSides; i++){
-          pub.points[i] = {
-            x: pub.x+(Math.cos(initAngle+angle*i)*pub.size), 
-            y: pub.y+(Math.sin(initAngle+angle*i)*pub.size) 
-          };
-        }
-      };
-      
-      var pub = {
-        x: x,
-        y: y,
-        size: size,
-        numSides: numSides,
-        rotateAngle: 0
-      };
-      
-      pub.draw = function(ctx){
-        ctx.strokeStyle = color;
-        ctx.lineWidth   = lineWidth;
-        ctx.fillStyle   = colorFill;
-        ctx.beginPath();
-        for(var index in pub.points){        
-          if(index == 0)
-            ctx.moveTo(pub.points[index].x, pub.points[index].y);
-          else
-            ctx.lineTo(pub.points[index].x, pub.points[index].y);            
-        }
-        ctx.fill();
-        if(lineWidth > 0){
-          ctx.lineTo(pub.points[0].x, pub.points[0].y);        
-          ctx.stroke();
-        }
-        if(debugON){
-          ctx.fillStyle = "red";
-          ctx.fillRect(pub.x-1, pub.y-1, 2, 2);
-        }
-      };
-      
-      pub.setColor = function (newColor){
-        color = newColor;
-      };
-      
-      pub.update = function () {
-        updatePoints();
-      };
-      
+    
+    var updatePoints = function(){
+      var angle   = deg2rad(360/pub.numSides);
+      var initAngle = deg2rad(pub.rotateAngle);
+      pub.points  = []; 
+      for(var i=0; i<pub.numSides; i++){
+        pub.points[i] = {
+          x: pub.x+(Math.cos(initAngle+angle*i)*pub.size), 
+          y: pub.y+(Math.sin(initAngle+angle*i)*pub.size) 
+        };
+      }
+    };
+    
+    var pub = {
+      x: x,
+      y: y,
+      size: size,
+      numSides: numSides,
+      rotateAngle: 0
+    };
+    
+    pub.draw = function(ctx){
+      ctx.strokeStyle = color;
+      ctx.lineWidth   = lineWidth;
+      ctx.fillStyle   = colorFill;
+      ctx.beginPath();
+      for(var index in pub.points){        
+        if(index == 0)
+          ctx.moveTo(pub.points[index].x, pub.points[index].y);
+        else
+          ctx.lineTo(pub.points[index].x, pub.points[index].y);            
+      }
+      ctx.fill();
+      if(lineWidth > 0){
+        ctx.lineTo(pub.points[0].x, pub.points[0].y);        
+        ctx.stroke();
+      }
+      if(debugON){
+        ctx.fillStyle = "red";
+        ctx.fillRect(pub.x-1, pub.y-1, 2, 2);
+      }
+    };
+    
+    pub.setColor = function (newColor){
+      color = newColor;
+    };
+    
+    pub.update = function () {
       updatePoints();
-      
-      return pub;      
-    }
-  };  
-})();
+    };
+    
+    updatePoints();
+    
+    return pub;      
+  }
 
-console.log("Primitives loaded...");
+})( Primitive );
+
+if(debug) console.log("Primitives loaded...");
