b85cd2472bdec7bcfdb169564f4c8ccb8ae7f489
Juan Miguel Rodriguez Cerón <juanmi.rodriguez.ceron@gmail.com>
2015-10-28 23:53:53 +0100

    Creating rts prototype with primitives.

--- main.js
+++ main.js
@@ -9,21 +9,32 @@ requirejs.config({
 requirejs(['polyfills','primitives', 'game'], function(p, Primitives, G){
 
   // Get a reference to the element.
-  var elem = document.getElementById('myCanvas');
+  var viewport = document.getElementById('myCanvas');
 
   // Always check for properties and methods, to make sure your code doesn't break 
   // in other browsers.
-  if ( elem && elem.getContext ) {
+  if ( viewport && viewport.getContext ) {
     // Get the 2d context.
     // Remember: you can only initialize one context per element.
-    window.context = elem.getContext( '2d' );
+    window.context = viewport.getContext( '2d' );
     if ( context ) {
 
-      var game = new G.Game(context, elem.width, elem.height);
+      var game = new G.Game(context, viewport.width, viewport.height);
+      game.backgroundColor = '#080808';
 
-      var p = new Primitives.Polygon(100, 100, 20, 5);
+      var goldmine = new Primitives.Polygon(100, 100, 10, 4);
+      goldmine.color = '#f0f010';
+      goldmine.rotateAngle = 45;
 
-      game.addItemToState(p);
+      var trees = [];
+      for(var i=0; i < 100; i++) {
+        trees.push(new Primitives.Polygon(Math.random()*viewport.width, Math.random()*viewport.height, 10, 3));
+        trees[trees.length-1].rotateAngle = 30;
+        game.addItemToState(trees[trees.length-1]);
+      }
+      debugger;
+
+      game.addItemToState(goldmine);
 
       game.loop();
     }
